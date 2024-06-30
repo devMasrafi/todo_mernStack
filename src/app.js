708d7473871deccd2943a73609b749a80c55cd01
Md. Masrafi Mondol <masrafisw@gmail.com>
2024-06-30 14:06:17 +0600

    server running

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({limit:"16kb", extended: true}))
 
- 
+app.use(cors({
+    origin: "*"
+}))
+
+app.use(cookieParser())
+ export { app }
